6d952da82734c2bda158e049d408dc68656d8d63
xiccli <xiccli@umich.edu>
2018-04-04 15:01:15 -0400

    modified

---
+++ portfolio/js/imgspc.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+           $("a.zoom").imgbox({
+             'speedIn':0,
+             'speedOut':0,
+             'allowMultiple' : false,
+             'autoScale':true,
+             'height':100%;
+           });
+
+       });
+
+// function openModal1() {
+//   document.getElementById('myModal').style.display = "block";
+// }
+//
+// function closeModal1() {
+//   document.getElementById('myModal').style.display = "none";
+// }
