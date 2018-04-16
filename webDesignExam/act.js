b12fabfb8fb0bd0db03ab09d12c6e698121f411f
xiccli <xiccli@umich.edu>
2018-04-16 11:24:58 -0400

    modified

--- webDesignExam/act.js
+++ webDesignExam/act.js
@@ -1,3 +1,10 @@
-$('btnhide').on('click',function(){
-  $('.image1').hide();
-})
+
+$('#btnstyle1').click(function (e) {
+    e.stopPropagation();
+    $('#img1').hide();
+});
+
+$('#btnstyle2').click(function (e) {
+    e.stopPropagation();
+    $('#img1').show();
+});
